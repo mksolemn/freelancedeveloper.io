15f16b20a1599698488cea31adf8fc534d3804a7
Mindaugas Kavaliauskas <mksolemn@gmail.com>
2018-07-12 09:09:14 +0200

    Initial master branch commit with angular instance

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'freedev-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scs']
+})
+export class AppComponent {
+  title = 'freedev';
+}
